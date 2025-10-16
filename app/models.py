555965ea2fb4389a5ea7209719c5021b302adf7f
SAEED SANDE <2103143@ritindia.edu>
2025-10-16 13:46:21 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from . import db
+from datetime import datetime
+
+class Account(db.Model):
+    __tablename__ = 'accounts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    balance = db.Column(db.Float, default=0.0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Account {self.email}>"
